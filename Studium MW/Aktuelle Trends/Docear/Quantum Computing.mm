9380658acb16e25c8fcb8653a0977961062f08ae
martin.schneider <martin.schneider@quodata.de>
2017-11-15 18:54:19 +0100

    Mindmap für Quantum Computing

---
+++ Studium MW/Aktuelle Trends/Docear/Quantum Computing.mm
@@ -0,0 +1,223 @@
+<map version="docear 1.1" project="15F9287BD954QDAU6BZJPGV8HZJFLPW7RY7" project_last_home="file:/D:/Docear/" dcr_id="1509991919298_2jen87k73z09qrygqaryb2wxm">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Quantum Computing" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1510063264474"><hook NAME="MapStyle" zoom="1.791">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<font SIZE="13" BOLD="true"/>
+<node POSITION="left" ID="ID_879711999" CREATED="1509992725511" MODIFIED="1510146812027" VSHIFT="-20" MOVED="1510130329462"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Anwendungen</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#ff0000" WIDTH="2"/>
+<attribute NAME="key" VALUE="Kinnear1999"/>
+<attribute NAME="year" VALUE="1999"/>
+<attribute NAME="title" VALUE="Advances in Genetic Programming, Band 3"/>
+<attribute NAME="authors" VALUE="Kenneth E. Kinnear"/>
+<node TEXT="Cryptographie" ID="ID_1475711451" CREATED="1509994223975" MODIFIED="1510060819371" MOVED="1509994238874" HGAP="60" VSHIFT="30">
+<node ID="ID_137575160" CREATED="1509994245855" MODIFIED="1510059977626" LINK="https://www.sciencedaily.com/releases/2001/12/011220081620.htm" HGAP="60"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.sciencedaily.com/releases/2001/12/011220081620.htm">Shor's Algorithmus</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<attribute NAME="key" VALUE="Division2001"/>
+<attribute NAME="journal" VALUE="ScienceDaily"/>
+<attribute NAME="year" VALUE="2001"/>
+<attribute NAME="title" VALUE="IBM&apos;s Test-Tube Quantum Computer Makes History: First Demonstration of Shor&apos;s Historic Factoring Algorithm"/>
+<attribute NAME="authors" VALUE="IBM Research Division"/>
+</node>
+<node TEXT="Quantenteleportation" ID="ID_431529755" CREATED="1509994257767" MODIFIED="1510059982251" HGAP="70" VSHIFT="10"/>
+</node>
+<node TEXT="Big Data" ID="ID_1551350882" CREATED="1509994216456" MODIFIED="1510060827142" HGAP="50" VSHIFT="-70"/>
+<node ID="ID_128085227" CREATED="1509994205539" MODIFIED="1510130305209" HGAP="50" VSHIFT="-20" MOVED="1510064562201"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="http://www.nature.com/nature/journal/v543/n7646_supp_out/fig_tab/543S2a_G3.html">Parallelverarbeitung</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook URI="project://15F9287BD954QDAU6BZJPGV8HZJFLPW7RY7/Bilder/Quantum_computing.jpg" SIZE="0.5529303" NAME="ExternalObject"/>
+</node>
+<node ID="ID_90997796" CREATED="1510138213644" MODIFIED="1510148384069"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="http://rsta.royalsocietypublishing.org/content/368/1924/3581">Simulation von Biomolek&#252;len</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Grundlagen" POSITION="left" ID="ID_388256446" CREATED="1510148914824" MODIFIED="1510150140969" HGAP="-10" VSHIFT="150" MOVED="1510149127179">
+<edge COLOR="#00ffff"/>
+<hook NAME="FreeNode"/>
+<font BOLD="true"/>
+<node TEXT="Quantenverschr&#xe4;nkung" ID="ID_1638272627" CREATED="1510148975925" MODIFIED="1510149184889" HGAP="140" VSHIFT="70" MOVED="1510149168766">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Superposition" ID="ID_1211927875" CREATED="1510149946948" MODIFIED="1510149952861"/>
+<node TEXT="Verschiedene Strukturen" ID="ID_1424271618" CREATED="1510149004244" MODIFIED="1510150134728" HGAP="40" VSHIFT="170" MOVED="1510150134727">
+<hook NAME="FreeNode"/>
+<node TEXT="Trapped ionic atoms" ID="ID_214719847" CREATED="1510149033578" MODIFIED="1510149148008" HGAP="-80" VSHIFT="40">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Elektronenspin" ID="ID_77719286" CREATED="1510149065823" MODIFIED="1510149142990" HGAP="-40" VSHIFT="70" MOVED="1510149142979">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Supraleiter" ID="ID_1895962728" CREATED="1510149085895" MODIFIED="1510149138047" HGAP="-10" VSHIFT="100" MOVED="1510149138035">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="Photonen" ID="ID_1008101838" CREATED="1510149096810" MODIFIED="1510149312800" HGAP="-10" VSHIFT="130" MOVED="1510149134114">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+</node>
+<node TEXT="Herausforderungen" POSITION="left" ID="ID_1692887631" CREATED="1509992736099" MODIFIED="1510146819869" HGAP="170" VSHIFT="20">
+<edge COLOR="#0000ff" WIDTH="2"/>
+<font BOLD="true"/>
+<node TEXT="Skalierung" ID="ID_1191601897" CREATED="1509993886753" MODIFIED="1510042406493"/>
+<node TEXT="Relaxation" ID="ID_1464106816" CREATED="1509993898646" MODIFIED="1510042385383"/>
+<node TEXT="Dekoh&#xe4;renz" ID="ID_77688654" CREATED="1510042387417" MODIFIED="1510042397434"/>
+<node TEXT="Algorithmen" ID="ID_53621129" CREATED="1510060759657" MODIFIED="1510060764222"/>
+<node TEXT="Fehlerkorrektur" ID="ID_1917764838" CREATED="1510064212487" MODIFIED="1510064230758"/>
+</node>
+<node POSITION="right" ID="ID_67245339" CREATED="1509992750480" MODIFIED="1510146803522" HGAP="-60" VSHIFT="-240" MOVED="1510063886312"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Aktueller Stand</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#00ff00" WIDTH="2"/>
+<node ID="ID_1385222924" CREATED="1509994308934" MODIFIED="1510146607086" HGAP="40" VSHIFT="343" MOVED="1509994341220"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.dwavesys.com/press-releases/d-wave-systems-previews-2000-qubit-quantum-system">Kommerzielle Quantencomputer</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Forschung" ID="ID_685682220" CREATED="1510146734180" MODIFIED="1510146744658" MOVED="1510146751551"/>
+<node TEXT="Optimierungen" ID="ID_1547076208" CREATED="1510146654108" MODIFIED="1510146700387"/>
+</node>
+<node TEXT="Multi-Qubit-Rechner f&#xfc;r spezialisierte Anwendungen" ID="ID_1463013756" CREATED="1509994406298" MODIFIED="1509994758194" HGAP="40" VSHIFT="20"/>
+<node ID="ID_1780746470" CREATED="1509994159847" MODIFIED="1510146614359" HGAP="40" VSHIFT="-58"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.research.ibm.com/ibm-q/learn/what-is-quantum-computing/">IBM Q</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook URI="project://15F9287BD954QDAU6BZJPGV8HZJFLPW7RY7/Bilder/Quantum_Leap_Supercomputer_Graphic_Online_Final_V11.jpg" SIZE="0.29016712" NAME="ExternalObject"/>
+</node>
+<node ID="ID_1706861993" CREATED="1510131131904" MODIFIED="1510146632731" HGAP="158" VSHIFT="659" MOVED="1510131155077"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.research.ibm.com/ibm-q/learn/what-is-quantum-computing/images/infogaphic-typesofquantum.jpg">Quelle</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="FreeNode"/>
+<hook URI="project://15F9287BD954QDAU6BZJPGV8HZJFLPW7RY7/Bilder/Quantum_computer_ways.jpg" SIZE="0.25" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</map>
